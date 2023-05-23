5b4b15ceb8ae22d9c369bffd947c44720c75569e
TheBackenders <ahmadyouseff1986@gmail.com>
2023-05-23 17:41:40 +0300

    add IndexPage and Views

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
+use app\controller\UserController;
+require(__DIR__."/app/controllers/userController.php");
+define('BASE_PATH', '/darbni/newbranch/Group4_repo/');
+$request=$_SERVER['REQUEST_URI'] ;
+if ($request === BASE_PATH) {  
+    $ucontroller = new UserController();
+        $ucontroller->searchuser();    
+    }
+    
+    elseif(strpos($request ,BASE_PATH.'deletuser/')===0){
+        $id = substr($request, strlen(BASE_PATH . 'deletuser/'));
+        $ucontroller = new UserController();
+        $ucontroller->deletuser($id);  
+
+    }
+   
+
+
+
 ?>
